4ad52914af7a1fd73fed118ab79ea78a688ffa3d
GuRuGu <peter.k.nets@gmail.com>
2019-04-28 14:48:09 +0300

    Refactor select routes

--- app_ejs_6/routes/index.js
+++ app_ejs_6/routes/index.js
@@ -6,11 +6,11 @@ const itemController = require("../controllers/itemController");
 
 // theme routes
 router.post("/theme/add", themeController.addTheme);
-router.get("/theme/:id", themeController.selectTheme);
+router.get("/theme/select/:id", themeController.selectTheme);
 router.get("/theme/delete/:id", themeController.deleteTheme);
 // item routes
 router.post("/item/add", itemController.saveItem);
-router.get("/item/:id", itemController.selectItem);
+router.get("/item/select/:id", itemController.selectItem);
 router.get("/item/delete/:itemId/:themeId", itemController.deleteItem);
 // app routes
 router.get("/", appController.mainPage);
