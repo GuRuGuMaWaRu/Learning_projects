fa7352502098f34ccda6f647f3af7c4b54e3499f
GuRuGu <peter.k.nets@gmail.com>
2019-04-30 11:29:38 +0300

    Add Cart and CurrencySelector into Header

--- app_react_2/src/Header/index.js
+++ app_react_2/src/Header/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Cart from "../Cart";
+import CurrencySelector from "../CurrencySelector";
+import "./index.css";
 
 const Header = () => {
   return (
-    <div>
+    <div className="Header">
       <h1>Delux Adventure</h1>
+      <Cart />
+      <CurrencySelector />
     </div>
   );
 };
