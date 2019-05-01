5bc76dd89d071f3979d2943b68026d4df23ec808
GuRuGu <peter.k.nets@gmail.com>
2019-05-01 15:50:59 +0300

    Add basic styling for products

--- app_react_2/src/Product/index.js
+++ app_react_2/src/Product/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Product = ({ product }) => {
-  return <div>{product.name}</div>;
+  return (
+    <div className="Product">
+      <img src="https://picsum.photos/200" alt={product.name} />
+      <div>{product.name}</div>
+      <div>
+        <strong>{product.price}</strong>
+      </div>
+      <button>Add to Cart</button>
+    </div>
+  );
 };
 
 export default Product;
--- app_react_2/src/ProductList/index.js
+++ app_react_2/src/ProductList/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Product from "../Product";
+import "./index.css";
 
 const ProductList = ({ products }) => {
   const createProductList = () => {
-    return products.map(product => <Product product={product} />);
+    return products.map(product => (
+      <Product key={product.name} product={product} />
+    ));
   };
+
   return <div className="ProductList">{createProductList()}</div>;
 };
 
