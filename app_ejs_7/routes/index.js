7e6a2afbd4ac20185bcdbca2d7d2e263f50d830f
GuRuGu <peter.k.nets@gmail.com>
2019-05-03 11:59:46 +0300

    Save shop and product data to DB

--- app_ejs_7/routes/index.js
+++ app_ejs_7/routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Shop = require("../models/Shop");
+const Product = require("../models/Product");
 
 router.get("/shop/create", (req, res) => {
   res.render("shopCreate");
@@ -13,7 +14,19 @@ router.post("/shop/create", async (req, res) => {
     description: data.description
   };
 
-  await Shop.create(shopData);
+  // save new shop
+  const savedShop = await new Shop(shopData).save();
+
+  // save new products for this shop
+  const products = data.products
+    .split(";")
+    .reduce((processedProducts, product) => {
+      const [name, price] = product.split(",").map(dataItem => dataItem.trim());
+
+      return [...processedProducts, { name, price, shop: savedShop._id }];
+    }, []);
+
+  await Product.insertMany(products);
 
   res.redirect("/");
 });
