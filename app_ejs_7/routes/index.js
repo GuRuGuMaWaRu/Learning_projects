64333c35284b6f7061f3f588329875fbf0c7b3b6
GuRuGu <peter.k.nets@gmail.com>
2019-05-03 09:44:00 +0300

    Add a simple Add New Shop form

--- app_ejs_7/routes/index.js
+++ app_ejs_7/routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/shop/create", (req, res) => {
+  res.render("shopCreate");
+});
+
 router.get("/", (req, res) => {
   res.render("home");
 });
