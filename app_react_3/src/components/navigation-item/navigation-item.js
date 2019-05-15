e60ef5d76fe7615f46576a518e6dc7569e8e4c64
GuRuGu <peter.k.nets@gmail.com>
2019-05-15 20:27:47 +0300

    Flesh out Navigation and NavigationItem

--- app_react_3/src/components/navigation-item/navigation-item.js
+++ app_react_3/src/components/navigation-item/navigation-item.js
@@ -1,7 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const NavigationItem = () => {
-  return <div>NavigationItem</div>;
+const NavigationItem = ({ path, onClick, isCurrent, children }) => {
+  return (
+    <a
+      href={path}
+      onClick={e => {
+        e.preventDefault();
+        onClick(path);
+      }}
+    >
+      {children}
+    </a>
+  );
+};
+
+NavigationItem.propTypes = {
+  path: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  isCurrent: PropTypes.bool.isRequired,
+  children: PropTypes.string.isRequired
 };
 
 export default NavigationItem;
--- app_react_3/src/components/navigation/navigation.js
+++ app_react_3/src/components/navigation/navigation.js
@@ -2,12 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import NavigationItem from "../navigation-item/navigation-item";
 
-const Navigation = ({ pages, currentPath }) => {
-  console.log(pages);
-  return <div>Navigation</div>;
+const Navigation = ({ navigate, pages, currentPath }) => {
+  const links = pages.map(({ path, title }) => (
+    <NavigationItem
+      key={path}
+      path={path}
+      onClick={navigate}
+      isCurrent={path === currentPath}
+    >
+      {title}
+    </NavigationItem>
+  ));
+
+  return <div>{links}</div>;
 };
 
 Navigation.propTypes = {
+  navigate: PropTypes.func.isRequired,
   pages: PropTypes.arrayOf(
     PropTypes.shape({
       path: PropTypes.string.isRequired,
