81856e08d623353b769e1d04860766fc96ae2314
GuRuGu <peter.k.nets@gmail.com>
2019-05-15 20:17:24 +0300

    Add PropTypes to Navigation component

--- app_react_3/src/components/navigation/navigation.js
+++ app_react_3/src/components/navigation/navigation.js
@@ -1,7 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
+import NavigationItem from "../navigation-item/navigation-item";
 
-const Navigation = () => {
+const Navigation = ({ pages, currentPath }) => {
+  console.log(pages);
   return <div>Navigation</div>;
+};
+
+Navigation.propTypes = {
+  pages: PropTypes.arrayOf(
+    PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  currentPath: PropTypes.string.isRequired
 };
 
 export default Navigation;
