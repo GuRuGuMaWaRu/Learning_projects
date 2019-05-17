5d587a4c8c94fe01980c07b2902f6253e4919fcd
GuRuGu <peter.k.nets@gmail.com>
2019-05-17 18:42:37 +0300

    Change Shopping into Class-based component

--- app_react_3/client/src/components/pages/shopping/shopping.js
+++ app_react_3/client/src/components/pages/shopping/shopping.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Shopping = () => {
-  return <div>Shopping</div>;
-};
+class Shopping extends Component {
+  componentDidMount() {
+    // run a saga to get shop names (or should I move this to a side menu where shops are displayed and selected?)
+  }
+
+  render() {
+    return <div>Shopping</div>;
+  }
+}
 
 export default Shopping;
