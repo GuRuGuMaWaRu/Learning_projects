1c2f79694502ac13182f4ec1e1d11280461b3c18
GuRuGu <peter.k.nets@gmail.com>
2019-05-18 13:55:04 +0300

    Add custom styles to App

--- app_react_3/client/src/components/app/app.js
+++ app_react_3/client/src/components/app/app.js
@@ -1,11 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 import Navigation from "../navigation/navigation.container";
+import styles from "./app.css";
 
 const App = ({ children }) => {
+  const appStyles = classNames("container", styles.container);
+
   return (
     <div>
       <Navigation />
-      {children}
+      <div className={appStyles}>{children}</div>
     </div>
   );
 };
