59338de73bcf1a3c9563b11103909476a594aff2
GuRuGu <peter.k.nets@gmail.com>
2019-05-18 13:44:03 +0300

    Create basic ShopEditForm component

---
+++ app_react_3/client/src/components/shop-edit-form/shop-edit-form.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ShopEditForm = () => {
+  return (
+    <form>
+      <div className="form-group">
+        <label for="shopNameInput">Shop name</label>
+        <input type="text" className="form-control" id="shopNameInput" />
+      </div>
+      <div className="form-group">
+        <label for="typeSelect">Type</label>
+        <select className="form-control" id="typeSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <label for="descriptionInput">Description</label>
+        <textarea className="form-control" id="descriptionInput" rows="3" />
+      </div>
+      <button className="btn btn-success">Save</button>
+    </form>
+  );
+};
+
+export default ShopEditForm;
