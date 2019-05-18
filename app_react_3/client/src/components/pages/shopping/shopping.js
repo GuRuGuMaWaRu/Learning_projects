02b75222818900ec86205f16139896f73a843b85
GuRuGu <peter.k.nets@gmail.com>
2019-05-18 13:21:17 +0300

    Change Shopping component back to function-based

--- app_react_3/client/src/components/pages/shopping/shopping.js
+++ app_react_3/client/src/components/pages/shopping/shopping.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 
-class Shopping extends Component {
-  componentDidMount() {
-    // run a saga to get shop names (or should I move this to a side menu where shops are displayed and selected?)
-  }
+const Shopping = () => {
+  // componentDidMount() {
+  //   // run a saga to get shop names (or should I move this to a side menu where shops are displayed and selected?)
+  // }
 
-  render() {
-    return <div>Shopping</div>;
-  }
-}
+  return <div>Shopping</div>;
+};
 
 export default Shopping;
