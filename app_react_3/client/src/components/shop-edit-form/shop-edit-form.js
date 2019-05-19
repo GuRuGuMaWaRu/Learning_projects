d823fb093caf25d1f40980fc1c8eea40aaecf67b
GuRuGu <peter.k.nets@gmail.com>
2019-05-19 15:32:34 +0300

    Move redux-saga into client & start playing with formik package

--- app_react_3/client/src/components/shop-edit-form/shop-edit-form.js
+++ app_react_3/client/src/components/shop-edit-form/shop-edit-form.js
@@ -1,29 +1,74 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import styles from "./shop-edit-form.css";
 
 const ShopEditForm = () => {
   const shopTypes = ["Weapons", "Magic", "Armor", "Travel gear", "Other"];
 
+  const test = e => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="shopNameInput">Shop name</label>
-        <input type="text" className="form-control" id="shopNameInput" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="typeSelect">Type</label>
-        <select className="form-control" id="typeSelect">
-          {shopTypes.map(shopType => (
-            <option key={shopType}>{shopType}</option>
-          ))}
-        </select>
-      </div>
-      <div className="form-group">
-        <label htmlFor="descriptionInput">Description</label>
-        <textarea className="form-control" id="descriptionInput" rows="3" />
-      </div>
-      <button className="btn btn-success">Save</button>
-    </form>
+    <Formik
+      initialValues={{ name: "", type: "Magic", description: "" }}
+      validate={values => {
+        let errors = {};
+        if (!values.name) {
+          errors.name = "Required";
+        }
+        return errors;
+      }}
+      onSubmit={(values, actions) => {
+        // actions.setSubmitting = false;
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field
+            name="name"
+            render={({ field }) => (
+              <div className="form-group">
+                <label htmlFor="shopNameInput">Shop name</label>
+                <input
+                  {...field}
+                  type="text"
+                  className="form-control"
+                  id="shopNameInput"
+                />
+              </div>
+            )}
+          />
+          <ErrorMessage
+            name="name"
+            render={msg => <div className={styles.error}>{msg}</div>}
+          />
+        </Form>
+      )}
+    </Formik>
   );
 };
 
 export default ShopEditForm;
+// {/* <form>
+// <div className="form-group">
+//   <label htmlFor="shopNameInput">Shop name</label>
+//   <input type="text" className="form-control" id="shopNameInput" />
+// </div>
+//   <div className="form-group">
+//     <label htmlFor="typeSelect">Type</label>
+//     <select className="form-control" id="typeSelect">
+//       {shopTypes.map(shopType => (
+//         <option key={shopType}>{shopType}</option>
+//       ))}
+//     </select>
+//   </div>
+//   <div className="form-group">
+//     <label htmlFor="descriptionInput">Description</label>
+//     <textarea className="form-control" id="descriptionInput" rows="3" />
+//   </div>
+//   <button className="btn btn-success" type="submit">
+//     Save
+//   </button>
+// </form> */}
