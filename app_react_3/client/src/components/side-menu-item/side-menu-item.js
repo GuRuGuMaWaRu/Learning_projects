ee93bb74fada930a0ab60af8d79b3e9d6d592a59
GuRuGu <peter.k.nets@gmail.com>
2019-05-24 15:03:12 +0300

    Style side menu items

--- app_react_3/client/src/components/side-menu-item/side-menu-item.js
+++ app_react_3/client/src/components/side-menu-item/side-menu-item.js
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./side-menu-item.css";
 
 const SideMenuItem = ({ shopName, onSelectShop }) => {
-  return <div onClick={onSelectShop}>{shopName}</div>;
+  return (
+    <div className="side-menu-item" onClick={onSelectShop}>
+      {shopName}
+    </div>
+  );
 };
 
 SideMenuItem.propTypes = {
