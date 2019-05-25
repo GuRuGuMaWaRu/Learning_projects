c08ca6572e423dce159561b3b3d71053af8a4601
GuRuGu <peter.k.nets@gmail.com>
2019-05-25 12:21:40 +0300

    Clear earlier selected shop until the new selected shop is loaded from DB

--- app_react_3/client/src/reducers/selectedShop.js
+++ app_react_3/client/src/reducers/selectedShop.js
@@ -1,4 +1,5 @@
 import {
+  SELECT_SHOP,
   SELECT_SHOP_SUCCESS,
   SELECT_SHOP_FAILURE
 } from "../actions/selectShop";
@@ -7,6 +8,8 @@ const initialState = {};
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case SELECT_SHOP:
+      return initialState;
     case SELECT_SHOP_SUCCESS:
       return action.shop;
     case SELECT_SHOP_FAILURE:
