8eeec53247ec9f554773f8635a164236308672af
GuRuGu <peter.k.nets@gmail.com>
2019-06-03 18:44:25 +0300

    Restructure cartItems state to register same items under the same index

--- app_react_3/client/src/reducers/cart.js
+++ app_react_3/client/src/reducers/cart.js
@@ -4,13 +4,40 @@ const initialState = {
   items: []
 };
 
+const addCartItem = (prevItems, newItem) => {
+  const isNewItem = prevItems.every(
+    prevItem => prevItem.itemId !== newItem.itemId
+  );
+
+  if (isNewItem) {
+    return [
+      ...prevItems,
+      {
+        ...newItem,
+        itemQty: 1
+      }
+    ];
+  } else {
+    return prevItems.map(item => {
+      if (item.itemId === newItem.itemId) {
+        return {
+          ...item,
+          itemQty: item.itemQty + 1
+        };
+      } else {
+        return item;
+      }
+    });
+  }
+};
+
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART_SUCCESS:
-      const { itemId, itemPrice, itemName } = action.itemData;
+      // const { itemId, itemPrice, itemName } = action.itemData;
       return {
         ...state,
-        items: [...state.items, { itemId, itemPrice, itemName }]
+        items: addCartItem(state.items, action.itemData)
       };
     default:
       return state;
