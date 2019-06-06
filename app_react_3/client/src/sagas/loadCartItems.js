269e182e4082ecb785f55463bca8b514f751d612
GuRuGu <peter.k.nets@gmail.com>
2019-06-06 16:58:32 +0300

    Create a saga for getting cart items

---
+++ app_react_3/client/src/sagas/loadCartItems.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { put, call, takeLatest } from "redux-saga/effects";
+import {
+  LOAD_CART_ITEMS,
+  loadCartItemsSuccess,
+  loadCartItemsFailure
+} from "../actions/loadCartItems";
+
+function* handleLoadCartItems(aciton) {
+  try {
+    const cartItems = yield call(axios.get, "/api/cart/get");
+    yield put(loadCartItemsSuccess(cartItems));
+  } catch (error) {
+    yield put(loadCartItemsFailure());
+  }
+}
+
+export default function*() {
+  yield takeLatest(LOAD_CART_ITEMS, handleLoadCartItems);
+}
