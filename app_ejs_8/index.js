2c479ec6d6c5a7deb4f04aa6cdc9492294024cf4
GuRuGu <peter.k.nets@gmail.com>
2019-06-20 13:30:23 +0300

    Create a sample ejs page

--- app_ejs_8/index.js
+++ app_ejs_8/index.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("All is working!");
+  res.render("main");
 });
 
 app.listen(port, () => {
