10c5e87403aab2e798be46d775fc72116d44da45
GuRuGu <peter.k.nets@gmail.com>
2019-06-20 15:47:07 +0300

    Add bodyParser to get submitted form data

--- app_ejs_8/index.js
+++ app_ejs_8/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,7 @@ app.set("views", "./views");
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("main");
@@ -14,6 +16,11 @@ app.get("/", (req, res) => {
 
 app.get("/item/create", (req, res) => {
   res.render("create-item-page");
+});
+
+app.post("/item/create", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
