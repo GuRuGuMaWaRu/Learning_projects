5fde5510f53c39be035af100e76e0843ab13463d
GuRuGu <peter.k.nets@gmail.com>
2019-06-24 10:41:01 +0300

    Run both server and client together

--- app_react_4/app.js
+++ app_react_4/app.js
@@ -3,6 +3,6 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 5000);
 
 module.exports = app;
