27f9bde6787d7ca21d316b22a11b3f46e4d11be5
GuRuGu <peter.k.nets@gmail.com>
2019-06-27 17:07:43 +0300

    Add basic creation form

--- app_react_4/client/src/components/create-entry/create-entry.js
+++ app_react_4/client/src/components/create-entry/create-entry.js
@@ -1,5 +1,42 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import Container from "@material-ui/core/Container";
 
 export default function CreateEntry() {
-  return <div>CreateEntry</div>;
+  return (
+    <Container>
+      <h2>Add New Journal Entry</h2>
+      <Formik
+        initialValues={{ title: "", body: "" }}
+        validate={values => {
+          let errors = {};
+          if (!values.title) {
+            errors.title = "Required";
+          }
+          if (!values.body) {
+            errors.body = "Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            console.log(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 1000);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="title" />
+            <ErrorMessage name="title" component="div" />
+            <Field component="textarea" name="body" />
+            <ErrorMessage name="body" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Add
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
 }
