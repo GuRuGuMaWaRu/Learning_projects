2d4bb00c02f82592476abf37138944162e4240dd
GuRuGu <peter.k.nets@gmail.com>
2019-06-28 19:03:56 +0300

    Update entries reducer to save new entries

--- app_react_4/client/src/reducers/entriesReducer.js
+++ app_react_4/client/src/reducers/entriesReducer.js
@@ -1,7 +1,11 @@
+import { createEntryAction } from "../actions";
+
 const initialState = [];
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case createEntryAction.CREATE_ENTRY_SUCCESS:
+      return [...state, action.createdEntry];
     default:
       return state;
   }
