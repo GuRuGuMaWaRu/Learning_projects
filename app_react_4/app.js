1808fd94a95f0f9cd99a2d008a44f3a8d5721029
GuRuGu <peter.k.nets@gmail.com>
2019-06-28 18:28:30 +0300

    Add body-parser

--- app_react_4/app.js
+++ app_react_4/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const entryRoutes = require("./routes/entryRoutes");
 
 require("dotenv").config();
 
 const app = express();
+
 app.set("port", process.env.PORT || 5000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/entry", entryRoutes);
 
 module.exports = app;
