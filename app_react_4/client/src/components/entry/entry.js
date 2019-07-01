02cf496b5edf190442d635b82d147c76428a0706
GuRuGu <peter.k.nets@gmail.com>
2019-07-01 16:37:36 +0300

    Style Entry page

--- app_react_4/client/src/components/entry/entry.js
+++ app_react_4/client/src/components/entry/entry.js
@@ -1,21 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   container: {
     marginTop: "2rem"
+  },
+  entry: {
+    padding: theme.spacing(3, 2),
+    marginTop: "2rem"
+  },
+  entryBody: {
+    margin: "2rem 0"
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "space-between"
   }
-});
+}));
 
 const Entry = ({ entry }) => {
   const classes = useStyles();
 
   return (
     <Container className={classes.container}>
-      <Typography variant="h4">{entry.title}</Typography>
+      <Typography variant="h4">Entry View</Typography>
+      <Paper className={classes.entry}>
+        <Typography variant="h6">{entry.title}</Typography>
+        <Typography className={classes.entryBody} variant="body1">
+          {entry.body}
+        </Typography>
+        <div className={classes.buttons}>
+          <Button variant="contained" color="primary">
+            Edit
+          </Button>
+          <Button variant="contained" color="secondary">
+            Delete
+          </Button>
+        </div>
+      </Paper>
     </Container>
   );
 };
