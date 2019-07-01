56ac04c5c8e52cba99eea060d7a4636f58691c98
GuRuGu <peter.k.nets@gmail.com>
2019-07-01 14:05:38 +0300

    Style list of entries

--- app_react_4/client/src/components/journal/journal.js
+++ app_react_4/client/src/components/journal/journal.js
@@ -4,10 +4,8 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
 import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
@@ -16,8 +14,17 @@ const useStyles = makeStyles(theme => ({
   },
   list: {
     width: "100%",
-    maxWidth: 360,
     backgroundColor: theme.palette.background.paper
+  },
+  listItem: {
+    "&:nth-child(odd)": {
+      backgroundColor: "#f5f9ff"
+    }
+  },
+  listContents: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center"
   }
 }));
 
@@ -25,8 +32,28 @@ const JournalWithStyles = ({ entries }) => {
   const classes = useStyles();
 
   const entryList = entries.map(entry => (
-    <ListItem key={entry._id} alignItems="flex-start">
-      {entry.title}
+    <ListItem
+      className={classes.listItem}
+      key={entry._id}
+      alignItems="center"
+      divider={true}
+    >
+      <ListItemText
+        primary={
+          <div className={classes.listContents}>
+            <span>
+              <b>Title:</b> {entry.title}
+            </span>
+            <Button
+              variant="outlined"
+              component="span"
+              className={classes.button}
+            >
+              More
+            </Button>
+          </div>
+        }
+      />
     </ListItem>
   ));
 
