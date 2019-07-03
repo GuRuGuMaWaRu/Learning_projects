a258b7f5c4b746ad7933b30efefa3ad4e1cf796b
GuRuGu <peter.k.nets@gmail.com>
2019-07-03 14:28:41 +0300

    Create basic express application

---
+++ app_react_5/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+
+// set up DB
+mongoose.set("useFindAndModify", false);
+mongoose.connect(
+  "mongodb+srv://GuRuGu:i44iYtewBM21noYb@cluster0-z8edg.mongodb.net/app_react_5",
+  { useNewUrlParser: true }
+);
+
+app.set("port", process.env.PORT || 5000);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+module.exports = app;
