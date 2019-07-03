60ca4eac974a72b6e828c4df6c16f15307cb73e0
GuRuGu <peter.k.nets@gmail.com>
2019-07-03 14:38:49 +0300

    Update routes file

--- app_react_5/routes/index.js
+++ app_react_5/routes/index.js
@@ -1,17 +1,17 @@
 module.exports = app => {
-  app.get("cafe/index", (req, res) => {
-    res.send();
+  app.get("/cafe/index", (req, res) => {
+    res.send("index");
   });
-  app.post("cafe/create", (req, res) => {
-    res.send();
+  app.post("/cafe/create", (req, res) => {
+    res.send("create");
   });
-  app.get("cafe/read", (req, res) => {
-    res.send();
+  app.get("/cafe/read", (req, res) => {
+    res.send("read");
   });
-  app.put("cafe/update", (req, res) => {
-    res.send();
+  app.put("/cafe/update", (req, res) => {
+    res.send("update");
   });
-  app.delete("cage/delete", (req, res) => {
-    res.send();
+  app.delete("/cafe/delete", (req, res) => {
+    res.send("delete");
   });
 };
