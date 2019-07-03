01a9be6d897d2561c2414a1dd75c63089797f7f8
GuRuGu <peter.k.nets@gmail.com>
2019-07-03 14:36:11 +0300

    Create basic routes file

--- app_react_5/app.js
+++ app_react_5/app.js
@@ -18,4 +18,6 @@ app.set("port", process.env.PORT || 5000);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+require("./routes")(app);
+
 module.exports = app;
---
+++ app_react_5/routes/index.js
@@ -0,0 +1,17 @@
+module.exports = app => {
+  app.get("cafe/index", (req, res) => {
+    res.send();
+  });
+  app.post("cafe/create", (req, res) => {
+    res.send();
+  });
+  app.get("cafe/read", (req, res) => {
+    res.send();
+  });
+  app.put("cafe/update", (req, res) => {
+    res.send();
+  });
+  app.delete("cage/delete", (req, res) => {
+    res.send();
+  });
+};
