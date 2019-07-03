152580254b3b899dc83378a3c5125bd11212e598
GuRuGu <peter.k.nets@gmail.com>
2019-07-03 15:45:41 +0300

    Add error handling to express app & handle test-vs-production db

--- app_react_5/app.js
+++ app_react_5/app.js
@@ -8,10 +8,13 @@ const app = express();
 
 // set up DB
 mongoose.set("useFindAndModify", false);
-mongoose.connect(
-  "mongodb+srv://GuRuGu:i44iYtewBM21noYb@cluster0-z8edg.mongodb.net/app_react_5",
-  { useNewUrlParser: true }
-);
+
+if (process.env.NODE_ENV !== "test") {
+  mongoose.connect(
+    "mongodb+srv://GuRuGu:i44iYtewBM21noYb@cluster0-z8edg.mongodb.net/app_react_5",
+    { useNewUrlParser: true }
+  );
+}
 
 app.set("port", process.env.PORT || 5000);
 
@@ -19,5 +22,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 require("./routes")(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message });
+});
 
 module.exports = app;
