90c630360c894540c6a8ce3a07dfeb2c92caa381
GuRuGu <peter.k.nets@gmail.com>
2019-07-04 12:34:32 +0300

    Write POST request test

--- app_react_5/test/controller.tests.js
+++ app_react_5/test/controller.tests.js
@@ -5,7 +5,18 @@ const app = require("../app");
 const Cafe = mongoose.model("cafe");
 
 describe("Cafe controller", () => {
-  it("POST request to /api/cafes creates a new cafe", done => {});
+  it("POST request to /api/cafes creates a new cafe", done => {
+    request(app)
+      .post("/api/cafes")
+      .send({ title: "Cafe 1", description: "Good cafe" })
+      .end(() => {
+        Cafe.find().then(cafes => {
+          assert(cafes.length === 1);
+          assert(cafes[0].title === "Cafe 1");
+          done();
+        });
+      });
+  });
   it("GET request to /api/cafes loads all existing cafes", done => {});
   it("GET request to /api/cafes/:id loads a particular cafe", done => {});
   it("PUT request to /api/cafes/:id updates a particular cafe", done => {});
