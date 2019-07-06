ba9d72a8be8c94b90d3d251c02315d7de9ab742c
GuRuGu <peter.k.nets@gmail.com>
2019-07-06 11:37:41 +0300

    Set up react-router

--- app_react_5/client/src/components/app/App.js
+++ app_react_5/client/src/components/app/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/create">Add Cafe</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Route exact path="/" render={() => <div>List of all cafes</div>} />
+          <Route
+            path="/create"
+            render={() => <div>Form to create a new cafe</div>}
+          />
+          <Route render={() => <div>Not Found</div>} />
+        </main>
+      </Router>
+    </>
+  );
 };
 
 export default App;
