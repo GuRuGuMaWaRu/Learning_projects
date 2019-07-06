dd80692c5bd8ebd7ad9a1598f844c9079c941661
GuRuGu <peter.k.nets@gmail.com>
2019-07-06 10:08:03 +0300

    Remove commented out code from tests

--- app_react_5/test/test_helpers.js
+++ app_react_5/test/test_helpers.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
 
 before(done => {
-  mongoose.connect(
-    "mongodb+srv://GuRuGu:i44iYtewBM21noYb@cluster0-z8edg.mongodb.net/app_react_5_test",
-    {
-      useNewUrlParser: true
-    }
-  );
+  mongoose.connect(process.env.DB_TEST, {
+    useNewUrlParser: true
+  });
   mongoose.connection
     .once("open", () => done())
     .on("error", err => console.warn(`Error: ${err}`));
@@ -16,25 +13,4 @@ beforeEach(done => {
   mongoose.connection.collections.caves.drop(() => {
     done();
   });
-  // const { caves } = mongoose.connection.collections;
-
-  // caves
-  //   .drop()
-  //   .then(() => done())
-  //   .catch(() => done());
 });
-
-// afterEach(done => {
-// const { caves } = mongoose.connection.collections;
-
-// caves
-//   .drop()
-//   .then(() => done())
-//   .catch(() => done());
-// });
-
-// after(done => {
-//   mongoose.connection.collections.caves.drop().then(() => {
-//     mongoose.connection.close(done);
-//   });
-// });
