9fba1446658d3d8b57a6b7053c3c092d81e63db0
GuRuGu <peter.k.nets@gmail.com>
2019-07-07 13:59:18 +0300

    Add outer Container for all pages & use Switch to show routes exclusively

--- app_react_5/client/src/components/app/App.js
+++ app_react_5/client/src/components/app/App.js
@@ -1,19 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import { Navbar } from "../navigation";
 
+const useStyles = makeStyles({
+  main: {
+    marginTop: "104px"
+  }
+});
+
 const App = () => {
+  const classes = useStyles();
+
   return (
     <>
       <Router>
         <Navbar />
         <main>
-          <Route exact path="/" render={() => <div>List of all cafes</div>} />
-          <Route
-            path="/create"
-            render={() => <div>Form to create a new cafe</div>}
-          />
-          <Route render={() => <div>Not Found</div>} />
+          <Container className={classes.main} maxWidth="sm">
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => <div>List of all cafes</div>}
+              />
+              <Route
+                path="/create"
+                render={() => <div>Form to create a new cafe</div>}
+              />
+              <Route render={() => <div>Not Found</div>} />
+            </Switch>
+          </Container>
         </main>
       </Router>
     </>
