ce400a90467e3ec16c17cbf89d815d2fa773e7b7
GuRuGu <peter.k.nets@gmail.com>
2019-07-11 10:41:13 +0300

    Create a basic router app

--- app_react_6/app.js
+++ app_react_6/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const appRouter = require("./routes");
 const app = express();
 
 // load environment variables
@@ -6,5 +7,8 @@ require("dotenv").config();
 
 // Set up port variable in express
 app.set("port", process.env.PORT || 5000);
+
+// Connect main router
+app.use("/api", appRouter);
 
 module.exports = app;
