ee45a19454c00e62e74059c4f2597290075566b5
GuRuGu <peter.k.nets@gmail.com>
2019-07-11 10:33:02 +0300

    Use environment variables to set up server port

--- app_react_6/app.js
+++ app_react_6/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const app = express();
 
+// load environment variables
+require("dotenv").config();
+
+// Set up port variable in express
+app.set("port", process.env.PORT || 5000);
+
 module.exports = app;
--- app_react_6/index.js
+++ app_react_6/index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000...");
+const server = app.listen(app.get("port"), () => {
+  console.log(`Server is running on port ${server.address().port}...`);
 });
