3bf5c4652f112a7b7b38569db6d2ac7040e07f67
GuRuGu <peter.k.nets@gmail.com>
2019-07-13 17:43:48 +0300

    Add dotenv and use environment variables

--- app_react_6/app.js
+++ app_react_6/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const appRouter = require("./routes");
+
 const app = express();
 
 // load environment variables
-require("dotenv").config();
+require("dotenv").config({ path: "process.env" });
+
+// connect to mongoDB
+mongoose.connect(process.env.DB, { useNewUrlParser: true });
 
 // Set up port variable in express
 app.set("port", process.env.PORT || 5000);
