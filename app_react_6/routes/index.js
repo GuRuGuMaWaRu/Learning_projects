e86342272915d94dded6fb43201b70949f683f38
GuRuGu <peter.k.nets@gmail.com>
2019-07-13 17:44:13 +0300

    Write placeholders for all routes

--- app_react_6/routes/index.js
+++ app_react_6/routes/index.js
@@ -6,8 +6,24 @@ router.use((req, res, next) => {
   next();
 });
 
-router.get("/", (req, res) => {
-  res.send("Hello!");
+router.get("/blogposts", (req, res) => {
+  res.send("Index");
+});
+
+router.post("/blogposts", (req, res) => {
+  res.send("Create");
+});
+
+router.get("/blogposts/:id", (req, res) => {
+  res.send("Read");
+});
+
+router.put("/blogposts/:id", (req, res) => {
+  res.send("Update");
+});
+
+router.delete("/blogposts/:id", (req, res) => {
+  res.send("Delete");
 });
 
 module.exports = router;
