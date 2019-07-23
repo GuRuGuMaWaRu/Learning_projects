ab3b772aab2096f8225cdcebcc85950c4acf7965
GuRuGu <peter.k.nets@gmail.com>
2019-07-23 14:18:29 +0300

    Add main element to wrap around all pages

--- app_react_6/client/src/components/app/App.js
+++ app_react_6/client/src/components/app/App.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import styled from "styled-components/macro";
 import { Navbar } from "../navbar";
+
+const StyledMain = styled.main`
+  width: 80%;
+  margin: 2rem auto;
+`;
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/list" render={() => <div>List</div>} />
-        <Route exact path="/create" render={() => <div>Create</div>} />
-        <Route exact path="/update" render={() => <div>Update</div>} />
-        <Route render={() => <div>404. Not Found.</div>} />
-      </Switch>
+      <StyledMain>
+        <Switch>
+          <Route exact path="/" render={() => <div>Home</div>} />
+          <Route exact path="/list" render={() => <div>List</div>} />
+          <Route exact path="/create" render={() => <div>Create</div>} />
+          <Route exact path="/update" render={() => <div>Update</div>} />
+          <Route render={() => <div>404. Not Found.</div>} />
+        </Switch>
+      </StyledMain>
     </Router>
   );
 };
