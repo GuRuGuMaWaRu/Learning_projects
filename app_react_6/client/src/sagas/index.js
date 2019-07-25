aaa119cea8e1964e1e45da39647b9299d822d119
GuRuGu <peter.k.nets@gmail.com>
2019-07-25 20:35:39 +0300

    Write getBlogposts saga

--- app_react_6/client/src/sagas/index.js
+++ app_react_6/client/src/sagas/index.js
@@ -1,7 +1,11 @@
 import { all, takeLatest } from "redux-saga/effects";
-import { createAction } from "../actions";
+import { createAction, indexAction } from "../actions";
 import createBlogpost from "./create.saga";
+import getBlogposts from "./index.saga";
 
 export default function*() {
-  yield all([takeLatest(createAction.CREATE_BLOGPOST, createBlogpost)]);
+  yield all([
+    takeLatest(createAction.CREATE_BLOGPOST, createBlogpost),
+    takeLatest(indexAction.GET_BLOGPOSTS, getBlogposts)
+  ]);
 }
---
+++ app_react_6/client/src/sagas/index.saga.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { put, call } from "redux-saga/effects";
+import { indexAction } from "../actions";
+
+export default function*() {
+  try {
+    const { data: blogposts } = yield call(axios.get("/api/blogposts"));
+    yield put(indexAction.getBlogpostsSuccess(blogposts));
+  } catch (err) {
+    yield put(indexAction.getBlogpostsFailure(err));
+  }
+}
