5de21fb37d266fb7f40932d00001dee3cde4fdb9
GuRuGu <peter.k.nets@gmail.com>
2019-07-28 15:40:06 +0300

    Style Blogpost Details buttons

--- app_react_6/client/src/components/blogpost-details/BlogpostDetails.js
+++ app_react_6/client/src/components/blogpost-details/BlogpostDetails.js
@@ -21,9 +21,51 @@ const StyledBody = styled.p`
   line-height: 1.6rem;
 `;
 
-const StyledLikes = styled.div`
-  text-align: right;
+const StyledDets = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   font-size: 1.5rem;
+  margin-top: 2rem;
+`;
+
+const StyledCommentsHeader = styled.h3`
+  text-align: center;
+`;
+
+const StyledButton = styled.button`
+  color: #222;
+  padding: 1rem 1.2rem;
+  border: none;
+  margin-right: 1rem;
+  transition: all 0.3s;
+  cursor: pointer;
+  outline-style: none;
+  &:hover {
+    box-shadow: 0px 11px 11px 0px #999;
+  }
+`;
+
+const StyledCommentButton = styled(StyledButton)`
+  background-color: #f2f2f2;
+  &:active {
+    background-color: #e3e3e3;
+  }
+`;
+
+const StyledEditButton = styled(StyledButton)`
+  background-color: #f2f2f2;
+  &:active {
+    background-color: #e3e3e3;
+  }
+`;
+
+const StyledDeleteButton = styled(StyledButton)`
+  color: #f2f2f2;
+  background-color: #ba2d65;
+  &:active {
+    background-color: #84163b;
+  }
 `;
 
 const BlogpostDetails = () => {
@@ -37,9 +79,17 @@ const BlogpostDetails = () => {
         <span>{moment(blogpost.date).format(`YYYY-MM-DD, HH:MM`)}</span>
       </StyledAuthor>
       <StyledBody>{blogpost.body}</StyledBody>
-      <StyledLikes>
-        <ion-icon name="thumbs-up" /> {blogpost.likes}
-      </StyledLikes>
+      <StyledDets>
+        <div>
+          <StyledCommentButton>Add Comment</StyledCommentButton>
+          <StyledEditButton>Edit</StyledEditButton>
+          <StyledDeleteButton>Delete</StyledDeleteButton>
+        </div>
+        <span>
+          <ion-icon name="thumbs-up" /> {blogpost.likes}
+        </span>
+      </StyledDets>
+      <StyledCommentsHeader>Comments</StyledCommentsHeader>
     </>
   );
 };
