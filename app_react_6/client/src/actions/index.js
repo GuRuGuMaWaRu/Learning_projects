760aa1da5e9bc07e25aec8544b5f42420db52d55
GuRuGu <peter.k.nets@gmail.com>
2019-08-09 15:49:02 +0300

    Write Like Blogpost action

--- app_react_6/client/src/actions/index.js
+++ app_react_6/client/src/actions/index.js
@@ -3,3 +3,4 @@ export { default as indexAction } from "./index-action";
 export { default as getAction } from "./get-action";
 export { default as createCommentAction } from "./create-comment-action";
 export { default as clearBlogpostAction } from "./clear-blogpost-action";
+export { default as likeBlogpostAction } from "./like-blogpost-action";
