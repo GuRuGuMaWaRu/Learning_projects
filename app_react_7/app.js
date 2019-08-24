7e5d50fa527861938d613424434e4b9aa73ed341
GuRuGu <peter.k.nets@gmail.com>
2019-08-24 20:09:34 +0300

    Connect mongoDB & update plans

--- app_react_7/app.js
+++ app_react_7/app.js
@@ -1,15 +1,24 @@
-var createError = require("http-errors");
-var express = require("express");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
 // load environment variables
 require("dotenv").config({ path: "process.env" });
+
+// connect to mongoDB
+if (process.env.NODE_ENV === "development") {
+  mongoose.connect(process.env.DB, { useNewUrlParser: true });
+  mongoose.connection.once("open", () =>
+    console.log("Connected to development DB.")
+  );
+}
 
 app.use(logger("dev"));
 app.use(express.json());
