435386b947d98f654d48cc7a1dda1f80be1048e6
GuRuGu <peter.k.nets@gmail.com>
2019-08-24 20:18:31 +0300

    Write Store & Product models

---
+++ app_react_7/models/Store.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+
+const ProductSchema = new Schema({
+  storeId: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+const StoreSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  products: {
+    type: [ProductSchema]
+  },
+  tags: {
+    type: [String],
+    default: ["undefined"]
+  }
+});
+
+module.exports = mongoose.model("store", StoreSchema);
