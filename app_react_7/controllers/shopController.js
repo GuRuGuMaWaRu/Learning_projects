2f996ee8e3bb92c5f0f963be70f562d6dc859827
GuRuGu <peter.k.nets@gmail.com>
2019-09-03 11:27:29 +0300

    Update get-all-shops test and route to use native Mongo driver

--- app_react_7/controllers/shopController.js
+++ app_react_7/controllers/shopController.js
@@ -1,10 +1,25 @@
 const Shop = require("../models/Shop");
-const queryString = require("query-string");
+const db = require("../db");
 
 module.exports = {
   index: async (req, res) => {
-    const shops = await Shop.find({}, "name tags");
-    res.send(shops);
+    const shops = [];
+
+    db.getDb()
+      .db()
+      .collection("shops")
+      .find({})
+      .project({ name: 1, tags: 1 })
+      .sort({ name: 1 })
+      .forEach(shopDoc => {
+        shops.push(shopDoc);
+      })
+      .then(result => {
+        console.log(shops);
+        res.status(200).json(shops);
+      });
+    // const shops = await Shop.find({}, "name tags");
+    // res.send(shops);
   },
   withTags: async (req, res) => {
     const tags = Object.values(req.query);
--- app_react_7/test/shop-get-all.test.js
+++ app_react_7/test/shop-get-all.test.js
@@ -2,10 +2,11 @@ const assert = require("assert");
 const request = require("supertest");
 const Shop = require("../models/Shop");
 const app = require("../app");
+const db = require("../db");
 
 describe("Shop controller", () => {
   it("should return all shops on GET request to /api/shop", done => {
-    const shop1 = new Shop({
+    const shop1 = {
       name: "Shop 1",
       description: "should return all shops on GET request to /api/shop",
       products: [
@@ -16,8 +17,8 @@ describe("Shop controller", () => {
         }
       ],
       tags: ["magic", "weapons"]
-    });
-    const shop2 = new Shop({
+    };
+    const shop2 = {
       name: "Shop 2",
       description: "should return again all shops on GET request to /api/shop",
       products: [
@@ -28,17 +29,22 @@ describe("Shop controller", () => {
         }
       ],
       tags: ["weapons"]
-    });
+    };
 
-    Promise.all([shop1.save(), shop2.save()]).then(() => {
-      request(app)
-        .get("/shop")
-        .expect(200)
-        .end((err, res) => {
-          assert(res.body.length === 2);
-          assert(Object.keys(res.body[0]).length === 3);
-          done();
-        });
-    });
+    db.getDb()
+      .db()
+      .collection("shops")
+      .insertMany([shop1, shop2])
+      .then(() => {
+        console.log("inserted many");
+        request(app)
+          .get("/shop")
+          .expect(200)
+          .end((err, res) => {
+            assert(res.body.length === 2);
+            assert(Object.keys(res.body[0]).length === 3);
+            done();
+          });
+      });
   });
 });
