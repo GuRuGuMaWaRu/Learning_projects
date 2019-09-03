e33d036d5e510231a862ed84f9370a8a4db1815e
GuRuGu <peter.k.nets@gmail.com>
2019-09-03 12:13:27 +0300

    Update gitignore

--- app_react_7/test/shop-get-shop.test.js
+++ app_react_7/test/shop-get-shop.test.js
@@ -5,6 +5,37 @@ const db = require("../db");
 
 describe("Shop controller", () => {
   it("should return a particular shop data on GET request to /api/shops/:id", done => {
-    done();
+    const shop1 = {
+      name: "Shop 1",
+      description: "should return all shops on GET request to /api/shop",
+      products: [
+        {
+          shopId: "this._id",
+          name: "Long sword +1",
+          price: 1200
+        }
+      ],
+      tags: ["magic", "weapons"]
+    };
+    const shop2 = {
+      name: "Shop 2",
+      description: "should return again all shops on GET request to /api/shop",
+      products: [
+        {
+          shopId: "this._id",
+          name: "Long sword",
+          price: 30
+        }
+      ],
+      tags: ["weapons"]
+    };
+
+    db.getDb()
+      .db()
+      .collection("shops")
+      .insertMany([shop1, shop2])
+      .then(() => {
+        done();
+      });
   });
 });
