e320bb7456b15cfab7e5f333be873212c7fd46e3
GuRuGu <peter.k.nets@gmail.com>
2019-09-20 16:45:34 +0300

    Write add-product-to-cart test

--- app_react_7/test/cart-add-product.test.js
+++ app_react_7/test/cart-add-product.test.js
@@ -1,8 +1,30 @@
 const assert = require("assert");
 const request = require("supertest");
+const mongodb = require("mongodb");
+const ObjectId = mongodb.ObjectId;
 const app = require("../app");
 const db = require("../db");
 
 describe("Cart controller", () => {
-  it("should add a product into cart on POST request to /cart", done => {});
+  it("should add a product into cart on POST request to /cart", done => {
+    request(app)
+      .post("/cart")
+      .send({
+        shopId: "dsafddsaf43244",
+        productId: "kjlkdj5kl54jlk",
+        qty: 1
+      })
+      .expect(201)
+      .end((err, res) => {
+        const id = res.body[0]._id;
+        db.getDb()
+          .db()
+          .collection("cart")
+          .findOne({ _id: ObjectId(id) })
+          .then(foundDoc => {
+            assert(foundDoc._id === id);
+            done();
+          });
+      });
+  });
 });
