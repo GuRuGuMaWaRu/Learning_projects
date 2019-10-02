027435648d07e45770c55b70d520c9b269190636
GuRuGu <peter.k.nets@gmail.com>
2019-10-02 18:33:00 +0300

    Write test and code for increast-cartitem-qty route

--- app_react_7/controllers/cartController.js
+++ app_react_7/controllers/cartController.js
@@ -37,5 +37,25 @@ module.exports = {
           error: err
         });
       });
+  },
+  increase: async (req, res) => {
+    db.getDb()
+      .db()
+      .collection("cartItems")
+      .findOneAndUpdate(
+        { _id: new ObjectId(req.params.id) },
+        { $inc: { qty: 1 } },
+        { returnNewDocument: true }
+      )
+      .then(result => {
+        res.status(200).json({
+          message: "Cart Item quantity increased",
+          updatedDocument: req.params.id
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
   }
 };
--- app_react_7/routes/cart.js
+++ app_react_7/routes/cart.js
@@ -5,5 +5,6 @@ const cartController = require("../controllers/cartController");
 // add product to cart
 router.post("/", cartController.save);
 router.delete("/:id", cartController.delete);
+router.patch("/increase/:id", cartController.increase);
 
 module.exports = router;
