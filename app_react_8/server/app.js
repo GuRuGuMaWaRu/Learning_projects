dbf18061e4356e982ea2d04d6185a996feb5d23a
GuRuGu <peter.k.nets@gmail.com>
2019-10-06 13:19:27 +0300

    Handle errors

--- app_react_8/server/app.js
+++ app_react_8/server/app.js
@@ -2,7 +2,24 @@ const express = require("express");
 const app = express();
 const pictureRouter = require("./routes/pictureRouter");
 
+// handle app routes
 app.use("/pictures", pictureRouter);
+
+// handle 404 errors
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+// handle all errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000...");
