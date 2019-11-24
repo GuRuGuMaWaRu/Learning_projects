428fdf5d3956f2b1a80571b03798699b3adb4102
GuRuGu <peter.k.nets@gmail.com>
2019-11-24 09:09:37 +0200

    Write basic server with temp routes

---
+++ app_react_9/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Swords API" });
+});
+
+// Routes
+app.get("/api/swords", (req, res) => {
+  res.status(200).json({ msg: "Swords route" });
+});
+app.get("/api/user", (req, res) => {
+  res.status(200).json({ msg: "User route" });
+});
+app.get("/api/auth", (req, res) => {
+  res.status(200).json({ msg: "Auth route" });
+});
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
