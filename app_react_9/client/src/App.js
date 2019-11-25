34ab58823d9ced87cb10703e80df6a7b297d449c
GuRuGu <peter.k.nets@gmail.com>
2019-11-25 08:47:50 +0200

    Add some basic routes

--- app_react_9/client/src/App.js
+++ app_react_9/client/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
-  return <div>React App 9</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
