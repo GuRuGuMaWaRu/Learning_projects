4441b19d3c4342641a540af40b4ad0ef2f720f6a
GuRuGu <peter.k.nets@gmail.com>
2019-12-06 12:56:08 +0200

    Solving merge issues

--- app_react_8/app.js
+++ app_react_8/app.js
@@ -45,7 +45,15 @@ app.use((error, req, res, next) => {
   });
 });
 
-app.listen(5000, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("Server is listening on port 5000...");
 });
 
